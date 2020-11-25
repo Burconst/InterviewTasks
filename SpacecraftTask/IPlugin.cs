f23787205c8110eb16d0ad0a8f6f60b614446a21
Konstantin <36280804+Burconst@users.noreply.github.com>
2020-11-25 22:27:19 +0300

    XML comments are added

--- SpacecraftTask/IPlugin.cs
+++ SpacecraftTask/IPlugin.cs
@@ -2,12 +2,30 @@
 
 namespace ds.test.impl
 {
+    /// <summary>
+    /// Базовый интерфейс плагина.
+    /// </summary>
     public interface IPlugin
     {
+        /// <summary>
+        /// Название плагина.
+        /// </summary>
         string PluginName { get; }
+        /// <summary>
+        /// Версия плагина.
+        /// </summary>
         string Version { get; }
+        /// <summary>
+        /// Изображение плагина.
+        /// </summary>
         System.Drawing.Image Image { get; }
+        /// <summary>
+        /// Описание плагина.
+        /// </summary>
         string Description { get; }
+        /// <summary>
+        /// Запуск плагина.
+        /// </summary>
         int Run(int input1, int input2);
     }
 }
--- SpacecraftTask/Plugins.cs
+++ SpacecraftTask/Plugins.cs
@@ -3,12 +3,20 @@ using System.Linq;
 
 namespace ds.test.impl
 {
+    /// <summary>
+    /// Статический класс для управления текущим набором плагинов.
+    /// </summary>
     public static class Plugins
     {
         private static readonly List<IPlugin> _plugins = new List<IPlugin>();
-
+        /// <summary>
+        /// Количество добавленных плагинов.
+        /// </summary>
         public static int PluginsCount => _plugins.Count;
-
+        
+        /// <summary>
+        /// Названия добавленных плагинов.
+        /// </summary>
         public static string[] GetPluginNames 
         {
             get 
@@ -16,12 +24,27 @@ namespace ds.test.impl
                 return (from plugin in _plugins select plugin.PluginName).ToArray<string>();
             }
         }
-
+        /// <summary>
+        /// Получение плагина по названию.
+        /// </summary>
+        /// <param name="pluginName">Название плагина.</param>
+        /// <returns></returns>
         public static IPlugin GetPlugin(string pluginName) => _plugins.Find(p => p.PluginName == pluginName);
-
+        /// <summary>
+        /// Добавить плагин.
+        /// </summary>
         public static void Add(IPlugin plugin) => _plugins.Add(plugin);
+        /// <summary>
+        /// Добавить набор плагинов.
+        /// </summary>
         public static void Add(IEnumerable<IPlugin> plugins) => _plugins.AddRange(plugins);
+        /// <summary>
+        /// Удалить плагин.
+        /// </summary>
         public static bool Remove(IPlugin plugin) => _plugins.Remove(plugin);
+        /// <summary>
+        /// Удалить набор плагинов.
+        /// </summary>
         public static bool Remove(IEnumerable<IPlugin> plugins)
         {
             bool isAllRemoved = true;
@@ -31,8 +54,17 @@ namespace ds.test.impl
             }
             return isAllRemoved;
         }
-
+        /// <summary>
+        /// Удалить плагин по имени.
+        /// </summary>
+        /// <param name="pluginName">Навание плагина.</param>
+        /// <returns></returns>
         public static bool Remove(string pluginName) => Remove(_plugins.Find(p => p.PluginName == pluginName));
+        /// <summary>
+        /// Удалить плагины по именам.
+        /// </summary>
+        /// <param name="pluginNames">Названия плагинов.</param>
+        /// <returns></returns>
         public static bool Remove(IEnumerable<string> pluginNames)
         {
             bool isAllRemoved = true;
@@ -42,7 +74,9 @@ namespace ds.test.impl
             }
             return isAllRemoved;
         }
-
+        /// <summary>
+        /// Очистить набор плагинов.
+        /// </summary>
         public static void Clear() => _plugins.Clear();
 
     }
